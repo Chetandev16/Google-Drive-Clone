d22e31332b60d0ef20f25e3af56dbfae9161f879
Chetandev16 <chetanpathak45@gmail.com>
2023-11-27 21:13:57 +0530

    added cleark auth

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">{children}</div>
+  );
+};
+
+export default AuthLayout;
---
+++ app/lib/init-user.tsx
@@ -0,0 +1,27 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/app/lib/db";
+
+export const initUser = async () => {
+  const user = await currentUser();
+
+  if (!user) return redirectToSignIn();
+
+  const profile = await db.user.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) return profile;
+
+  const newProfile = await db.user.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
