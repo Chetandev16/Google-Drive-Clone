9136f43f3dcbe7f4667ed75e0d302ed66a8b404a
Chetandev16 <chetanpathak45@gmail.com>
2023-11-29 19:33:22 +0530

    comment removed in landing page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -9,12 +9,6 @@ import logo from "@/assets/logo.svg";
 import mainImage from "@/assets/mainpage.jpg";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 const LandingPage = () => {
   const { setTheme } = useTheme();
@@ -98,23 +92,6 @@ const LandingPage = () => {
           ©Google Drive Clone - Built for Educational Purposes
         </p>
       </div>
-      {/* <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu> */}
     </div>
   );
 };
